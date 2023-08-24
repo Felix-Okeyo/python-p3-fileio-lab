4b20abb660f124c5444d362db41f2dad21113a47
Felix-Okeyo <felixokeyo20@gmail.com>
2023-08-24 12:36:16 +0300

    completed lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,34 @@
 def write_file(file_name, file_content):
-    pass
+    """
+    Write the given content to a file with the provided name.
+
+    Args:
+        file_name (str): The name of the file to be written.
+        file_content (str): The content to be written to the file.
+    """
+    with open(f'{file_name}.txt', mode='w', encoding='utf-8') as file:   
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    """
+    Append the given content to an existing file with the provided name.
+
+    Args:
+        file_name (str): The name of the file to which content will be appended.
+        append_content (str): The content to be appended to the file.
+    """
+    with open(f'{file_name}.txt', mode='a', encoding='utf-8') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    """
+    Read and return the content of the file with the provided name.
+
+    Args:
+        file_name (str): The name of the file to be read.
+
+    Returns:
+        str: The content of the file.
+    """
+    with open(f'{file_name}.txt', mode='r', encoding='utf-8') as file:
+        return file.read()
